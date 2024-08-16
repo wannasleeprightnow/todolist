6db1d609d979b78fb08e3b0d0e86db621837dadf
wannasleep <wannasleeprightnowww@gmail.com>
2024-08-17 00:32:12 +0500

    fix: Change auth header font

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,9 +1,10 @@
 import SignInForm from "../components/SignInForm/SignInForm.tsx";
+import classes from "./SignInUp.module.css";
 
 export default function SignIn() {
   return (
     <>
-      <h1 style={{ textAlign: "center", marginTop: "20%" }}>TODOlist</h1>
+      <h1 className={classes.form_header}>TODOlist</h1>
       <SignInForm />
     </>
   );
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,9 +1,10 @@
 import SignUpForm from "../components/SignUpForm/SignUpForm.tsx";
+import classes from "./SignInUp.module.css";
 
 export default function SignIn() {
   return (
     <>
-      <h1 style={{ textAlign: "center", marginTop: "20%" }}>TODOlist</h1>
+      <h1 className={classes.form_header}>TODOlist</h1>
       <SignUpForm />
     </>
   );
